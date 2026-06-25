cc0613b0b1d13d537059efb3b41e68c48a24dce0
GOPINATH S <gopinath2963_bcs27@mepcoeng.ac.in>
2026-06-25 14:18:11 +0530

    Time: 7 ms (78.02%), Space: 12.5 MB (17.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x<0):
+            return False
+
+        s=str(x)
+        if s == s[::-1]:
+            return True
+        else:
+            return False
