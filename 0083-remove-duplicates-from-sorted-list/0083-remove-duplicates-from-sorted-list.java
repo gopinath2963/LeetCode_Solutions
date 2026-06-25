c795feb534d3acc143280ca7ba00b34de3c3e1d7
GOPINATH S <gopinath2963_bcs27@mepcoeng.ac.in>
2026-06-25 14:12:07 +0530

    Time: 0 ms (100%), Space: 45.6 MB (32.04%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                // skip duplicate
+                current.next = current.next.next;
+            } else {
+                // move forward
+                current = current.next;
+            }
+        }
+        
+        return head;
+    }
+}
